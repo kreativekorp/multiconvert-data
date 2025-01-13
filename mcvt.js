56a5e1c3010fa81ff23f51feb5024b8b18c45bf7
Rebecca Bettencourt <beckiergb@gmail.com>
2025-01-12 18:19:48 -0800

    Refactor command execution

--- mcvt.js
+++ mcvt.js
@@ -61,6 +61,91 @@ function printItems(items) {
 	console.log('');
 }
 
+// COMMANDS AND THEIR UTILITIES
+
+function glob(s) {
+	let r = '^';
+	for (let i = 0; i < s.length; i++) {
+		switch (s[i]) {
+			case '?': r += '.'; break;
+			case '*': r += '.*'; break;
+			case '.': case '+': case '^': case '$': r += '\\' + s[i]; break;
+			case '(': case ')': case '[': case ']': r += '\\' + s[i]; break;
+			case '{': case '}': case '|': case '\\': r += '\\' + s[i]; break;
+			default: r += s[i]; break;
+		}
+	}
+	return new RegExp(r + '$');
+}
+
+function listItems(regexp, logerr) {
+	const collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
+	const compare = (a,b) => collator.compare(a['type'], b['type']) || collator.compare(a['key'], b['key']);
+	const keys = Object.keys(index.index).filter(k => k.match(regexp));
+	const arrays = keys.map(k => index.index[k].filter(v => v['key'] === k));
+	const items = arrays.reduce((a,b) => a.concat(b), []).sort(compare);
+	if (items.length) {
+		printItems(items);
+		return true;
+	} else {
+		if (logerr) console.log('Found no objects with matching ids.');
+		return false;
+	}
+}
+
+function lookupItems(q, logerr, trace) {
+	const items = index.lookup(q);
+	if (items) {
+		printItems(items);
+		return true;
+	} else try {
+		const u = unit.parse(q);
+		index.build({'type': 'unit'}, 'en', {[q]: u});
+		printItems(index.lookup(q));
+		return true;
+	} catch (e) {
+		if (logerr) console.log(trace ? e : e.message);
+		return false;
+	}
+}
+
+function tokenToId(token) {
+	if ('id' in token) return token['id'];
+	if ('value' in token) return token['value'];
+	return token['image'];
+}
+
+function printContext(context) {
+	const collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
+	const keys = Object.keys(context).sort(collator.compare);
+	for (const k of keys) {
+		const ks = k.match(/^([\p{L}\p{M}][\p{L}\p{M}\p{N}]*)$/u) ? k : ('`' + k + '`');
+		console.log(ks + ' := ' + ops.str(context[k]));
+	}
+}
+
+function evaluateArray(tokens, context, trace) {
+	try {
+		const expr = interp.parse(tokens);
+		const result = interp.evaluate(expr, context);
+		const values = (ops.type(result) === 'array') ? result : [result];
+		let i = 0; for (const v of values) context['$' + (++i)] = v;
+		context['$0'] = result; context['$@'] = values; context['$#'] = i;
+	} catch (e) {
+		console.log(trace ? e : e.message);
+	}
+}
+
+function evaluate(tokens, context, trace) {
+	try {
+		const expr = interp.parse(tokens);
+		const result = interp.evaluate(expr, context);
+		console.log(ops.str(result));
+	} catch (e) {
+		console.log(trace ? e : e.message);
+	}
+}
+
 // COMMAND LINE INTERPRETER
 
 function execute(command, context) {
@@ -68,56 +153,53 @@ function execute(command, context) {
 		process.exit(0);
 		return;
 	}
-	if (command.match(/^[a-z]?[*]$/)) {
-		const prefix = command.substring(0, command.length - 1);
-		const collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
-		const compare = (a,b) => collator.compare(a['type'], b['type']) || collator.compare(a['key'], b['key']);
-		const keys = Object.keys(index.index).filter(k => k.startsWith(prefix));
-		const arrays = keys.map(k => index.index[k].filter(v => v['key'] === k));
-		const items = arrays.reduce((a,b) => a.concat(b), []).sort(compare);
-		if (items.length) printItems(items);
-		else console.log('Found no objects whose ids start with \'' + prefix + '\'.');
+	if (command === 'list' || command.startsWith('list ')) {
+		listItems(glob(command.substring(5).trim() || '*'), true);
 		return;
 	}
-	// First try to look up objects directly.
-	const items = index.lookup(command);
-	if (items) {
-		printItems(items);
+	if (command === 'lookup' || command.startsWith('lookup ')) {
+		const q = command.substring(7).trim();
+		if (!q) listItems(glob('*'), true);
+		else lookupItems(q, true, ops.bool(context['trace']));
 		return;
-	} else try {
-		const u = unit.parse(command);
-		index.build({'type': 'unit'}, 'en', {[command]: u});
-		printItems(index.lookup(command));
-		return;
-	} catch (e) {
-		// ignore
 	}
-	// Lexicalize the input. If the input is a single identifier, try again to look up objects directly.
-	const tokens = interp.lex(command);
-	if (tokens.length === 0) {
-		return;
-	} else if (tokens.length === 1 && tokens[0]['id']) {
-		const items2 = index.lookup(tokens[0]['id']);
-		if (items2) {
-			printItems(items2);
-			return;
-		} else try {
-			const u = unit.parse(tokens[0]['id']);
-			index.build({'type': 'unit'}, 'en', {[tokens[0]['id']]: u});
-			printItems(index.lookup(tokens[0]['id']));
-			return;
-		} catch (e) {
-			// ignore
+	if (command === 'clear' || command.startsWith('clear ')) {
+		const tokens = interp.lex(command.substring(6).trim());
+		if (!tokens.length) {
+			for (const k of Object.keys(context)) delete context[k];
+		} else {
+			for (const token of tokens) delete context[tokenToId(token)];
 		}
+		return;
 	}
-	// Parse the input as an expression.
-	try {
-		const expr = interp.parse(tokens);
-		const result = interp.evaluate(expr, context);
-		console.log(ops.str(result));
-	} catch (e) {
-		console.log(ops.bool(context['trace']) ? e : e.message);
+	if (command === 'unset' || command.startsWith('unset ')) {
+		const tokens = interp.lex(command.substring(6).trim());
+		for (const token of tokens) delete context[tokenToId(token)];
+		return;
 	}
+	if (command === 'set' || command.startsWith('set ')) {
+		const tokens = interp.lex(command.substring(4).trim());
+		if (!tokens.length) printContext(context);
+		else evaluateArray(tokens, context, ops.bool(context['trace']));
+		return;
+	}
+	if (command === 'print' || command.startsWith('print ')) {
+		const tokens = interp.lex(command.substring(6).trim());
+		if (!tokens.length) console.log();
+		else evaluate(tokens, context, ops.bool(context['trace']));
+		return;
+	}
+	if (command.startsWith('?') || command.startsWith('=')) {
+		const tokens = interp.lex(command.substring(1).trim());
+		if (!tokens.length) console.log();
+		else evaluate(tokens, context, ops.bool(context['trace']));
+		return;
+	}
+	if (lookupItems(command)) return;
+	const tokens = interp.lex(command);
+	if (!tokens.length) return;
+	if (tokens.length === 1 && tokens[0]['id'] && lookupItems(tokens[0]['id'])) return;
+	evaluate(tokens, context, ops.bool(context['trace']));
 }
 
 function shell(context) {
